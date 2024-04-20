42c22f51af158d9fac536fa3ee871a011c9684dd
Nico Robertson <nichorobertson1@gmail.com>
2024-04-20 19:07:58 +0100

    Part 2(c) completed

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,91 +1,87 @@
-const Header = ({ course: { name } }) => {
-  return (<h1>{name}</h1>)
+import { useState } from 'react'
+
+const Filter = ({ props: { filterString, handleFilterInput } }) => {
+  return (
+    <> Filter: <input value={filterString} onChange={handleFilterInput} /> </>
+  )
 }
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
-
-const Part = ({ part: { name, exercises } }) =>
-  <p>
-    {name} {exercises}
-  </p>
-
-
-const Content = ({ parts }) => {
-
-  const sum = parts.reduce((total, part) => total + part.exercises, 0)
-  
+const Form = ({ states, handlers }) => {
   return (
     <>
-      {parts.map((part) =>
-        <Part key={part.id} part={part} />
-      )}
-      <b>Total parts in course: {sum}<br/></b>
+      <h2>Add a person</h2>
+      <form>
+        <div>
+          name: <input value={states.newName} onChange={(e) => handlers.setNewName(e.target.value)} />
+        </div>
+        <div>
+          phone number: <input value={states.newPhoneNumber} onChange={(e) => handlers.setNewPhoneNumber(e.target.value)} />
+          <button type="submit" onClick={handlers.handleAddPerson}>add</button>
+        </div>
+      </form>
     </>
   )
 }
 
-const Course = ({ course }) => {
-
+const Persons = ({ filteredContacts }) => {
   return (
-    <>
-      <Header course={course} />
-      <Content parts={course.parts} />
-    </>
+    filteredContacts.map((person) => <p key={person.name}>{person.name} - {person.phoneNumber}</p>)
   )
 }
-
-
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phoneNumber: '040-123456' },
+    { name: 'Ada Lovelace', phoneNumber: '39-44-5323523' },
+    { name: 'Dan Abramov', phoneNumber: '12-43-234345' },
+    { name: 'Mary Poppendieck', phoneNumber: '39-23-6423122' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newPhoneNumber, setNewPhoneNumber] = useState('')
+  const [filterString, setFilterString] = useState('')
+  const [filteredContacts, setFilteredContacts] = useState(persons)
+
+  const handleAddPerson = (e) => {
+
+    e.preventDefault()
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} already exists in the phonebook`)
+      setNewName('')
     }
-  ]
-  
+    else if (persons.some((person) => person.phoneNumber === newPhoneNumber)) {
+      alert(`The number ${newPhoneNumber} already exists for a contact in your phonebook`)
+      setNewPhoneNumber('')
+    }
+    else {
+      const personToAdd = {
+        name: newName,
+        phoneNumber: newPhoneNumber
+      }
+      setPersons(persons.concat(personToAdd))
+      setNewName('')
+      setNewPhoneNumber('')
+      if (personToAdd.name.includes(filterString)) {
+        setFilteredContacts(filteredContacts.concat(personToAdd))
+      }
+    }
+
+  }
+
+  const handleFilterInput = (e) => {
+    setFilterString(e.target.value)
+    setFilteredContacts(persons.filter((person) => person.name.toUpperCase().includes((e.target.value).toUpperCase())))
+  }
+
+
   return (
-    <>
-    {courses.map((course) => <Course course={course} />)}
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <Filter props={{ filterString, handleFilterInput }} />
+      <Form states={{ newName, newPhoneNumber }} handlers={{ setNewName, setNewPhoneNumber, handleAddPerson }} />
+      <h2>Numbers</h2>
+      <Persons filteredContacts={filteredContacts} />
+    </div>
   )
 }
 
