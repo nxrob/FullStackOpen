d8aaae65e66f22cb5ecc5e81619883535617ae59
Nico Robertson <nichorobertson1@gmail.com>
2024-04-20 20:53:22 +0100

    Added axios functionality to phonebook

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Filter = ({ props: { filterString, handleFilterInput } }) => {
   return (
@@ -25,22 +26,30 @@ const Form = ({ states, handlers }) => {
 
 const Persons = ({ filteredContacts }) => {
   return (
-    filteredContacts.map((person) => <p key={person.name}>{person.name} - {person.phoneNumber}</p>)
+    filteredContacts.map((person) => <p key={person.name}>{person.name} - {person.number}</p>)
   )
 }
 
+//****************************-> APP <-********************************
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phoneNumber: '040-123456' },
-    { name: 'Ada Lovelace', phoneNumber: '39-44-5323523' },
-    { name: 'Dan Abramov', phoneNumber: '12-43-234345' },
-    { name: 'Mary Poppendieck', phoneNumber: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhoneNumber, setNewPhoneNumber] = useState('')
   const [filterString, setFilterString] = useState('')
   const [filteredContacts, setFilteredContacts] = useState(persons)
+
+  useEffect(() => {
+    console.log("effect")
+    axios.get('http://localhost:3001/persons')
+        .then(r => {
+          console.log("Promised fulfilled with response: ", r.data)
+          setPersons(r.data)
+          setFilteredContacts(r.data)
+        })
+  }, [])
+
+  console.log("rendering ", persons.length, " people")
 
   const handleAddPerson = (e) => {
 
@@ -49,14 +58,14 @@ const App = () => {
       alert(`${newName} already exists in the phonebook`)
       setNewName('')
     }
-    else if (persons.some((person) => person.phoneNumber === newPhoneNumber)) {
+    else if (persons.some((person) => person.number === newPhoneNumber)) {
       alert(`The number ${newPhoneNumber} already exists for a contact in your phonebook`)
       setNewPhoneNumber('')
     }
     else {
       const personToAdd = {
         name: newName,
-        phoneNumber: newPhoneNumber
+        number: newPhoneNumber
       }
       setPersons(persons.concat(personToAdd))
       setNewName('')
